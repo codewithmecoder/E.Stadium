6f0627c90f24207bd96f2ee3823e2b744d101451
cheanith2909 <sokcheanith1031@hotmail.com>
2022-08-12 16:38:52 +0700

    cookie

--- E.Stadium.Api/Program.cs
+++ E.Stadium.Api/Program.cs
@@ -66,7 +66,7 @@ app.UseRouting();
 app.UseCors(x => x
                 .AllowAnyMethod()
                 .AllowAnyHeader()
-                .SetIsOriginAllowed(origin => true) // allow any origin
+                .SetIsOriginAllowed(origin => origin == "https://estadium.org") // allow any origin
                 .AllowCredentials()); // allow credentials
 
 app.UseAuthentication();
