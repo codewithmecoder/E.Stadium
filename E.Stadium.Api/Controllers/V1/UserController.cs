1c318c970d0def812ffe115875e85021791aae7f
cheanith2909 <sokcheanith1031@hotmail.com>
2022-08-12 15:21:33 +0700

    cookie

--- E.Stadium.Api/Controllers/V1/UserController.cs
+++ E.Stadium.Api/Controllers/V1/UserController.cs
@@ -81,7 +81,6 @@ public class UserController : BaseController
         await _userService.StoreToken(user, token.AccessToken, _protector);
         Response.Cookies.Append("Authorization", $"{token.AccessToken}", new CookieOptions
         {
-            MaxAge = TimeSpan.FromDays(7),
             HttpOnly = true,
             SameSite = SameSiteMode.None,
             Secure = true
